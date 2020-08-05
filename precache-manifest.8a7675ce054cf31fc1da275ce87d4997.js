3be159213bc5003c1aa70ef0d7c1b807bbb63fad
Your Name <you@example.com>
2020-08-04 18:18:28 -0700

    Updates

---
+++ precache-manifest.8a7675ce054cf31fc1da275ce87d4997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95725749e9380815ce9f3b22f1aa2892",
+    "url": "/univation/index.html"
+  },
+  {
+    "revision": "1be3fe80e5b0fc0f6afd",
+    "url": "/univation/static/css/main.47966c06.chunk.css"
+  },
+  {
+    "revision": "c0fd42e69ce7efe2c858",
+    "url": "/univation/static/js/2.a6292bbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/univation/static/js/2.a6292bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be3fe80e5b0fc0f6afd",
+    "url": "/univation/static/js/main.8b85eeca.chunk.js"
+  },
+  {
+    "revision": "d75da7a56ba56e397a0e",
+    "url": "/univation/static/js/runtime-main.85023b74.js"
+  }
+]);
